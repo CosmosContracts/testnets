223b8afa57b6b76aaffff5bafe927a80b0a4d1fd
Alex Lynham <alex@lynh.am>
2021-12-09 08:35:44 +0000

    WIP: add astarte-2 testnet

---
+++ astarte-2/add_genaccs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+junod add-genesis-account  10000000000ujunox
+junod add-genesis-account  10000000000ujunox
+junod add-genesis-account  10000000000ujunox
+junod add-genesis-account  10000000000ujunox
+junod add-genesis-account  10000000000ujunox
