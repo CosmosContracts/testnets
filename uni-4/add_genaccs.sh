0003aaa8ed12db2d76acfd7ee0f87f2db33dcd83
Alex Lynham <alex@lynh.am>
2022-08-08 12:59:29 +0100

    add uni-4 draft instrux

---
+++ uni-4/add_genaccs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# roguenet faucet
+junod add-genesis-account juno1ntycamuz37p0mt8zrvamnvwrqax58xh5h06h8v 10000000000ujunox
+
+# deus labs faucet
+junod add-genesis-account juno16p9pe6ckrege6vpxdca0m5y93lz5j5zv0d9448 10000000000ujunox
+
+# vals go here
+junod add-genesis-account  10000000000ujunox
