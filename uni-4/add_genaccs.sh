a855fffe56bba6ade5e9fa4ecb4a3ce0d1bc3a08
Alex Lynham <alex@lynh.am>
2022-09-20 15:27:07 +0100

    also provide uusdcx to 3rd party faucets

--- uni-4/add_genaccs.sh
+++ uni-4/add_genaccs.sh
@@ -2,9 +2,11 @@
 
 # roguenet faucet
 junod add-genesis-account juno1ntycamuz37p0mt8zrvamnvwrqax58xh5h06h8v 10000000000ujunox
+junod add-genesis-account juno1ntycamuz37p0mt8zrvamnvwrqax58xh5h06h8v 1000000000000uusdcx
 
 # deus labs faucet
 junod add-genesis-account juno16p9pe6ckrege6vpxdca0m5y93lz5j5zv0d9448 10000000000ujunox
+junod add-genesis-account juno16p9pe6ckrege6vpxdca0m5y93lz5j5zv0d9448 1000000000000uusdcx
 
 # vals go here
 junod add-genesis-account  10000000000ujunox
