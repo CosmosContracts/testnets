e9a3b1f81b87b74988d4e80271eefd09e04fd329
Alex Lynham <alex@lynh.am>
2023-02-02 10:09:48 +0000

    update gentx date and genesis time

--- scripts/validate-gentx.sh
+++ scripts/validate-gentx.sh
@@ -18,7 +18,7 @@ start="2021-10-11 01:00:00Z"
 stTime=$(date --date="$start" +%s)
 
 # Gentx End date
-end="2023-02-01 17:00:00Z"
+end="2023-02-03 17:00:00Z"
 # Compute the seconds since epoch for end date
 endTime=$(date --date="$end" +%s)
 
